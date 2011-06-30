0763df72c9db3e51384a34e225298e55f04151ec
Gregory Haynes <greg@greghaynes.net>
2011-06-30 16:43:26 -0700

    Created starter blink code with makefile

---
+++ src/blink.c
@@ -0,0 +1,25 @@
+/* Taken from http://paul.graysonfamily.org/thoughts/avrlinux/ */
+
+#define F_CPU 80000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void delayms(uint16_t millis) {
+	while ( millis ) {
+		_delay_ms(1);
+		millis--;
+	}
+}
+
+int main(void) {
+	DDRB |= 1<<PB0;
+	while(1) {
+		PORTB &= ~(1<<PB0); /* LED on */
+		delayms(100);
+		PORTB |= 1<<PB0; /* LED off */
+		delayms(900);
+	}
+	return 0;
+}
+
