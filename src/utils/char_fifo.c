1e9ea0c5a27e8d24e183c3fbad4158e7d0e27f23
Gregory Haynes <greg@greghaynes.net>
2011-07-20 14:21:37 -0700

    Make char_fifo indexes wrap around

--- src/utils/char_fifo.c
+++ src/utils/char_fifo.c
@@ -17,21 +17,24 @@ uint8_t char_fifo_is_full(struct char_fifo *cf) {
 	return cf->push_ndx == (cf->pop_ndx - 1);
 }
 
+uint8_t char_fifo_inc_ndx(struct char_fifo *cf, uint8_t ndx) {
+	if(ndx == cf->buff_size) return 0;
+	else return ++ndx;
+}
+
 uint8_t char_fifo_push(struct char_fifo *cf, char ch) {
 	if(char_fifo_is_full(cf))
 		return 1;
-
 	cf->buff[cf->push_ndx] = ch;
-	++cf->push_ndx;
+	cf->push_ndx = char_fifo_inc_ndx(cf, cf->push_ndx);
 	return 0;
 }
 
 uint8_t char_fifo_pop(struct char_fifo *cf, char *ch) {
 	if(char_fifo_is_empty(cf))
 		return 1;
-
 	*ch = cf->buff[cf->pop_ndx];
-	++cf->pop_ndx;
+	cf->pop_ndx = char_fifo_inc_ndx(cf, cf->pop_ndx);
 	return 0;
 }
 
