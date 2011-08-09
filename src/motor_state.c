04cd357f689f0554484ebcf89cca6a81bba31f13
Gregory Haynes <greg@greghaynes.net>
2011-08-08 17:23:30 -0700

    Motor state seq only stores one uint8_t per state, motor_state_next works

--- src/motor_state.c
+++ src/motor_state.c
@@ -1,49 +1,55 @@
 #include "motor_state.h"
 #include "motor.h"
 
-// Sequence of states (A, B, C, Sense)
-static uint8_t motor_state_seq[6][4] = {
-	{
-		MOTOR_WINDING_STATE_FLOAT, // A
-		MOTOR_WINDING_STATE_POS, // B
-		MOTOR_WINDING_STATE_NEG, // C
-		MOTOR_WINDING_A // Sense Winding
-	}, {
-		MOTOR_WINDING_STATE_POS,
-		MOTOR_WINDING_STATE_FLOAT,
-		MOTOR_WINDING_STATE_NEG,
-		MOTOR_WINDING_B
-	}, {
-		MOTOR_WINDING_STATE_POS,
-		MOTOR_WINDING_STATE_NEG,
-		MOTOR_WINDING_STATE_FLOAT,
-		MOTOR_WINDING_C
-	}, {
-		MOTOR_WINDING_STATE_FLOAT,
-		MOTOR_WINDING_STATE_NEG,
-		MOTOR_WINDING_STATE_POS,
-		MOTOR_WINDING_A
-	}, {
-		MOTOR_WINDING_STATE_NEG,
-		MOTOR_WINDING_STATE_FLOAT,
-		MOTOR_WINDING_STATE_POS,
-		MOTOR_WINDING_B
-	}, {
-		MOTOR_WINDING_STATE_NEG,
-		MOTOR_WINDING_STATE_POS,
-		MOTOR_WINDING_STATE_FLOAT,
-		MOTOR_WINDING_C
-	} };
+static uint8_t cur_state_ndx;
 
+#define MOTOR_STATE(a, b, c, s)  (a | (b << 2) | (c << 4) | (s << 6))
+
+// Sequence of states (A, B, C, Sense)
+static uint8_t motor_state_seq[6] = {
+	MOTOR_STATE(MOTOR_WINDING_STATE_FLOAT, // A
+	            MOTOR_WINDING_STATE_POS, // B
+	            MOTOR_WINDING_STATE_NEG, // C
+	            MOTOR_WINDING_A), // Sense Winding
+	MOTOR_STATE(MOTOR_WINDING_STATE_POS,
+	            MOTOR_WINDING_STATE_FLOAT,
+	            MOTOR_WINDING_STATE_NEG,
+	            MOTOR_WINDING_B),
+	MOTOR_STATE(MOTOR_WINDING_STATE_POS,
+	            MOTOR_WINDING_STATE_NEG,
+	            MOTOR_WINDING_STATE_FLOAT,
+	            MOTOR_WINDING_C),
+	MOTOR_STATE(MOTOR_WINDING_STATE_FLOAT,
+	            MOTOR_WINDING_STATE_NEG,
+	            MOTOR_WINDING_STATE_POS,
+	            MOTOR_WINDING_A),
+	MOTOR_STATE(MOTOR_WINDING_STATE_NEG,
+	            MOTOR_WINDING_STATE_FLOAT,
+	            MOTOR_WINDING_STATE_POS,
+	            MOTOR_WINDING_B),
+	MOTOR_STATE(MOTOR_WINDING_STATE_NEG,
+	            MOTOR_WINDING_STATE_POS,
+	            MOTOR_WINDING_STATE_FLOAT,
+	            MOTOR_WINDING_C)
+};
+
+void motor_state_start(void) {
+	cur_state_ndx = 5;
+	motor_state_next();
+}
 
 uint8_t motor_state_create(uint8_t a,
                            uint8_t b,
                            uint8_t c) {
-	return a | (b << 2) | (c << 4);
+	return MOTOR_STATE(a, b, c, 0);
 }
 
 
-uint8_t motor_state_next(void) {
-	return 0;
+void motor_state_next(void) {
+	if(cur_state_ndx >= 5)
+		cur_state_ndx = 0;
+	else
+		++cur_state_ndx;
+	motor_set_state(motor_state_seq[cur_state_ndx]);
 }
 
--- src/motor_state.h
+++ src/motor_state.h
@@ -1,6 +1,9 @@
 #ifndef CONVIK_MOTOR_STATE_H
 #define CONVIK_MOTOR_STATE_H
 
+/* Code which controls the sequential stepping between motor states
+ * for use during open loop startup and closed loop */
+
 #include <avr/io.h>
 
 enum motor_winding_state_t {
@@ -9,11 +12,16 @@ enum motor_winding_state_t {
 	MOTOR_WINDING_STATE_NEG = 2
 };
 
+/* Call to set state 0 */
+void motor_state_start(void);
+
+/* Create a uint8_t representing the states of all three coils */
 uint8_t motor_state_create(uint8_t a,
                            uint8_t b,
                            uint8_t c);
 
-uint8_t motor_state_next(void);
+/* Advance the motor windings to the next state */
+void motor_state_next(void);
 
 #endif
 
