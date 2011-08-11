189d02428fd8d4968aa087776a796d526ee72947
Gregory Haynes <greg@greghaynes.net>
2011-08-11 01:11:59 -0700

    Switch between motor states based on ADC1 input

--- src/core/adc.c
+++ src/core/adc.c
@@ -7,6 +7,10 @@
 #include "adc.h"
 
 void adc_init(void) {
-	
+	// Enable ADC, set prescaler to 4
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+	// ADC Mux
+	ADMUX = (1<<ADLAR) | (1<<MUX0);
+
 }
 
--- src/main.c
+++ src/main.c
@@ -19,17 +19,67 @@ void delayms(uint16_t millis) {
 	}
 }
 
+static uint8_t state;
+
+void all_off(void) {
+	PORTB &= ~((1<<PB0) | (1<<PB1) | (1<<PB2));
+	PORTC &= ~(1<<PC0);
+	PORTD &= ~((1<<PD6) | (1<<PD7));
+}
+
+void next_state(void) {
+	if(state >= 5)
+		state = 0;
+	else
+		++state;
+
+	all_off();
+	switch(state) {
+		case 0:
+			PORTB |= (1<<PB2);
+			PORTD |= (1<<PD6);
+			break;
+		case 1:
+			PORTC |= (1<<PC0);
+			PORTD |= (1<<PD6);
+			break;
+		case 2:
+			PORTC |= (1<<PC0);
+			PORTD |= (1<<PD7);
+			break;
+		case 3:
+			PORTD |= (1<<PD7);
+			PORTB |= (1<<PB1);
+			break;
+		case 4:
+			PORTB |= (1<<PB1);
+			PORTB |= (1<<PB0);
+			break;
+		case 5:
+			PORTB |= (1<<PB2);
+			PORTB |= (1<<PB0);
+			break;
+	}
+}
+
 int main(void) {
-	DDRB |= 1<<PB0;
-	usart_init();
-	pwm_init();
-	pwm_set(0);
-	char ch;
+	DDRB = (1<<DDB0) | (1<<DDB1) | (1<<DDB2);
+	DDRC = (1<<DDC0);
+	DDRD = (1<<DDD6) | (1<<DDD7);
+
+	adc_init();
+	DIDR0 |= (1<<ADC1D);
+	uint8_t adc_val;
 
 	while(1) {
-		if(char_fifo_is_empty(usart_recv_fifo()))
-			char_fifo_pop(usart_recv_fifo(), &ch);
-			usart_send_char(ch);
+		next_state();
+		// start ADC
+		ADCSRA |= (1<<ADSC);
+		// Wait for ADC Complete
+		while(ADCSRA & (1<<ADSC));
+		adc_val = ADCH;
+		
+		delayms(adc_val);
 	}
 	return 0;
 }
